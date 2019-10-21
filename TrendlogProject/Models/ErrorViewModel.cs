2e00abb74023ecf2b9758474ae67f1fc1e38f9bf
Rasmus Bach Jørgensen <rasm74e5@edu.eal.dk>
2019-10-21 11:32:01 +0200

    Add project files.

---
+++ TrendlogProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrendlogProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
